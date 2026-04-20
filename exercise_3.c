98bf848fa08c5b7d3de1dac3a44a2f81131fb116
deleuzea <axel.deleuze@gmail.com>
2026-04-20 10:54:42 +0200

    Finished exercise 3

--- exercise_3.c
+++ exercise_3.c
@@ -44,4 +44,54 @@ void lbm_comm_ghost_exchange_ex3(lbm_comm_t * comm, lbm_mesh_t * mesh)
 	//example to access cell
 	//double * cell = lbm_mesh_get_cell(mesh, local_x, local_y);
 	//double * cell = lbm_mesh_get_cell(mesh, comm->width - 1, 0);
+
+	// Computing neighbors rank
+	int left_rank = (comm->rank_x > 0) ? comm->rank_x - 1 : MPI_PROC_NULL;
+	int right_rank = (comm->rank_x < comm->nb_x - 1) ? comm->rank_x + 1 : MPI_PROC_NULL;
+
+	// Standardize tag to factorize
+	int tag_to_left = 0;
+	int tag_to_right = 1;
+
+	// Size of a column, accounting the fact that each cell contains DIRECTIONS times doubles
+	int col_size = comm->height * DIRECTIONS;
+
+	// Initializing request and status array
+	int request_count = 0;
+	MPI_Request requests[4];
+	MPI_Status statuses[4];
+
+	/* ---------------------------------------------------------------
+	left to right : send from left neighbor | reception in the right neighbor
+		Note : we send from column width-2 as column width - 1 is a ghost one
+			   we receive from column width = 0 as its the left ghost cells column
+	--------------------------------------------------------------- */
+
+	// We send directly the whole column ! It's way more elegant and efficient that using a loop like I first wanted to do
+	MPI_Isend(lbm_mesh_get_cell(mesh, comm->width - 2, 0),
+			 col_size, MPI_DOUBLE,
+			 right_rank, tag_to_right,
+			 comm->communicator, &requests[request_count++]);
+
+	MPI_Irecv(lbm_mesh_get_cell(mesh, 0, 0),
+			 col_size, MPI_DOUBLE,
+			 left_rank, tag_to_right,
+			 comm->communicator, &requests[request_count++]);
+
+	/* ---------------------------------------------------------------
+	right to left : send from right neighbor | reception in the left neighbor
+	Note : we send from column 1 as column width = 0 is a ghost one
+		   we receive from column width - 1 as its the right ghost cells column
+	--------------------------------------------------------------- */
+	MPI_Isend(lbm_mesh_get_cell(mesh, 1, 0),
+			 col_size, MPI_DOUBLE,
+			 left_rank, tag_to_left,
+			 comm->communicator,&requests[request_count++]);
+
+	MPI_Irecv(lbm_mesh_get_cell(mesh, comm->width - 1, 0),
+			 col_size, MPI_DOUBLE,
+			 right_rank, tag_to_left,
+			 comm->communicator, &requests[request_count++]);
+
+	MPI_Waitall(request_count, requests, statuses);
 }
